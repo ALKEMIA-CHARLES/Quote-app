ed8e609152b956bdd8003967b05b0ea5057d1a49
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-10-25 17:22:45 +0300

    HTML

--- src/app/maincard/maincard.component.ts
+++ src/app/maincard/maincard.component.ts
@@ -16,15 +16,40 @@ export class MaincardComponent implements OnInit {
       "Charles Mtawali Jr",
       "Do what inspires you ",
       0,
-      0,
+      0
     )),
     (this.new_quote = new Quotes(
-      "Abel Mohamed", "ሁልጊዜ አዎንታዊ ለመሆን ሁልጊዜ ያስታውሱ", 0, 0
+      "Raphael Katana",
+      "Slack ME", 0, 0
     )),
-    (this.new_quote = new Quotes("Liang Hao", "成为您遇到的每个人的尽头", 0, 0)),
-    (this.new_quote = new Quotes("Beatrice Labelle", "Vous devez vous convaincre que vous êtes le passé, le présent et le futur", 0,0)),
+    (this.new_quote = new Quotes(
+      "Boyd Ndonga", "I love failing, it is absolutely okay to fail", 0, 0
+    )),
+    (this.new_quote = new Quotes(
+      "Abel Mohamed",
+      "ሁልጊዜ አዎንታዊ ለመሆን ሁልጊዜ ያስታውሱ",
+      0,
+      0
+    )),
+    (this.new_quote = new Quotes(
+      "Liang Hao",
+      "成为您遇到的每个人的尽头",
+      0,
+      0
+    )),
+    (this.new_quote = new Quotes(
+      "Beatrice Labelle",
+      "Vous devez vous convaincre que vous êtes le passé, le présent et le futur",
+      0,
+      0
+    )),
     (this.new_quote = new Quotes("Michael Jackson", "Heal the world", 0, 0)),
-    (this.new_quote = new Quotes("Michael Jordan", "form is temporary, class is permanent", 0, 0)),
+    (this.new_quote = new Quotes(
+      "Michael Jordan",
+      "form is temporary, class is permanent",
+      0,
+      0
+    )),
     (this.new_quote = new Quotes("Steve Jobs", "Let's change the World", 0, 0)),
     (this.new_quote = new Quotes("Pablo Escobar", "Plata O Plomo", 0, 0))
   ];
